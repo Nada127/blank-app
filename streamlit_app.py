1344dd49df5db07c3e40a09d683e9603fde30b95
nada mohammed <61509627+Nada127@users.noreply.github.com>
2024-09-18 20:10:58 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
-Area=None
+st.sidebar.title('hgvcd')
+area=None
 st.header("calculate Area")
 choose=st.selectbox('choose the shape',['circle','rectangle'])
 if choose=='circle':
   r=st.number_input('enter the radius',min_value=1,max_value=100)
-  Area=r*r*3.14
+  area=r*r*3.14
 elif choose=='rectangle':
   w=st.number_input('enter the width',min_value=1,max_value=100)
   h=st.number_input('enter the hight',min_value=1,max_value=100)
