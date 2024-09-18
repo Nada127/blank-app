8edf1dc19911344b61b4b5a065259000dd98a41d
nada mohammed <61509627+Nada127@users.noreply.github.com>
2024-09-18 21:34:52 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,22 +1,31 @@
 import pandas as pd
-import plotly as px
-import streamlit as st
-st.header('file upload app 2')
-file =st.file_uploader('upload dataset',type=['csv'])
-if file is not None:
-  df=pd.read_csv(file)
-  st.write(df)
-  num_row=st.slider('choose num rows',min_value=1,max_value=len(df))
-  names_column=st.multiselect('choose columns',df.columns.to_list())
-  st.write(df[:num_row][names_column])
-  if names_column:
-    st.write(df[:num_row][names_column])
-  else:
-    st.write(df[:num_row])
-num_col=df.select_dtypes(include='number').column.to_list()
-x_col=st.selectbox('choose x axis',num_col)
-y_col=st.selectbox('choose y axis',num_col)
-color=st.selectbox('choose color',df.columns.to_list())
-fig=px.scatter(x=x_col,y=y_col,color=color)
-st.pyplot_chart(fig)
+import plotly.express as px
 
+st.header('file upload app 2')
+file = st.file_uploader('upload dataset',type=['csv'])
+if file is not None:
+  df = pd.read_csv(file)
+  st.write(df)
+
+num_row = st.slider('choose num rows',min_value=1,max_value=len(df),step=1)
+names_column = st.multiselect('choose names of columns',df.columns.to_list())
+st.write(df[:num_row][names_column])
+
+if names_column:
+  st.write(df[:num_row][names_column])
+else:
+  st.write(df[:num_row])
+
+num_col = df.select_dtypes(include='number').columns.to_list()
+
+col1,col2,col3 = st.columns(3)
+with col1:
+ x_col = st.selectbox('choose x column',num_col)
+with col2:
+  y_col = st.selectbox('choose y column',num_col)
+with col3:
+  color = st.selectbox('choose color',df.columns.to_list())
+
+
+fig = px.scatter(df,x=x_col,y=y_col,color=color)
+st.plotly_chart(fig)
