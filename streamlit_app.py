2490ad6879d268626ac40ba0c924cd66bed9c71e
nada mohammed <61509627+Nada127@users.noreply.github.com>
2024-09-18 21:08:39 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,11 @@ st.header('file upload app 2')
 file =st.file_uploader('upload dataset',type=['csv'])
 if file is not None:
   df=pd.read_csv(file)
-  st.write(df) 
-num_row=st.slider('choose num rows',min_value=1,max_value=len(df))
-names_column=st.multiselect('choose columns',df.columns.to_list())
-st.write(df[:num_row][names_column])
-if names_column:
+  st.write(df)
+  num_row=st.slider('choose num rows',min_value=1,max_value=len(df))
+  names_column=st.multiselect('choose columns',df.columns.to_list())
   st.write(df[:num_row][names_column])
-else:
-  st.write(df[:num_row])
+  if names_column:
+    st.write(df[:num_row][names_column])
+  else:
+    st.write(df[:num_row])
