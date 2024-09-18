10e3cb08117b03827f5d9a6b0ceac1daa08a46f5
nada mohammed <61509627+Nada127@users.noreply.github.com>
2024-09-18 20:52:43 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,6 @@ st.header('file upload app 2')
 file =st.file_uploader('upload dataset',type=['csv'])
 if file is not None:
   df=pd.read_csv(file)
-  st.write(df)
+  st.write(df) 
+num_row=st.slider('choose num rows',min_value=1,max_value=len(df))
+st.write(df.head(num_row))
