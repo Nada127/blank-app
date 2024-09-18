e510e9c0918fa91151e65c657c2d936bd2301612
nada mohammed <61509627+Nada127@users.noreply.github.com>
2024-09-18 21:27:51 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,13 +6,13 @@ file =st.file_uploader('upload dataset',type=['csv'])
 if file is not None:
   df=pd.read_csv(file)
   st.write(df)
-num_row=st.slider('choose num rows',min_value=1,max_value=len(df))
-names_column=st.multiselect('choose columns',df.columns.to_list())
-st.write(df[:num_row][names_column])
-if names_column:
+  num_row=st.slider('choose num rows',min_value=1,max_value=len(df))
+  names_column=st.multiselect('choose columns',df.columns.to_list())
   st.write(df[:num_row][names_column])
-else:
-  st.write(df[:num_row])
+  if names_column:
+    st.write(df[:num_row][names_column])
+  else:
+    st.write(df[:num_row])
 num_col=df.select_dtypes(include='number').column.to_list()
 x_col=st.selectbox('choose x axis',num_col)
 y_col=st.selectbox('choose y axis',num_col)
